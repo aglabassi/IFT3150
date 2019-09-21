58719ef65ed6d6db60450c1d0a9c948b0c4bd576
Unknown <abdel.ghani.labassi@umontreal.ca>
2019-09-21 17:35:18 -0400

    First version of B_embeddor

--- B_embeddor.py
+++ B_embeddor.py
@@ -11,20 +11,49 @@ of low dimensionnality
 
 """
 
+from sklearn.manifold import SpectralEmbedding
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
 class B_embeddor():
   
-  def __init__(self, emb_dim, sigma ):
+    def __init__(self, embedding_dim=10, n_neighbors=300, sigma=1):
+        similarity_calculator = B_embeddor._get_similarity_calculator(n_neighbors, sigma)
+        self.embedding = SpectralEmbedding(n_components=embedding_dim, affinity=similarity_calculator)
     
-    self.emb_dim = emb_dim
-    self.sigma = sigma
+    def __call__(self,X):
+        B_hat = self.embedding.fit_transform(X)
+        return np.sign(B_hat)    
+        
+    def _get_similarity_calculator(n_neighbors, sigma):
+        
+        def similarity_calculator(X):
+            neighbors = NearestNeighbors(n_neighbors=n_neighbors, algorithm='ball_tree').fit(X)
+            _, neighbors = neighbors.kneighbors(X)
+            
+            n_samples = X.shape[0]
+            similarities = np.zeros((n_samples,n_samples))
+            
+            for i in range(n_samples):
+                for j in range(i,n_samples):
+                    if j in neighbors[i]: #Reduce running time of eigenmap
+                            norm = np.linalg.norm((X[i]-X[j]))
+                            similarities[i,j] = similarities[j,i] = np.exp(-norm/(2*(sigma**2)))
+                            
+            return similarities
+        
+        return similarity_calculator
+                    
+            
+            
+            
+            
+      
+      
+      
   
   
-  def train(self,X):
+     
+     
     
-    pass
-  
-  
-  def embed(self,X):
-    
-    pass
 
