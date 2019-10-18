b5926b329604b5be38dffbb95b330e5e6b030edb
Unknown <abdel.ghani.labassi@umontreal.ca>
2019-10-18 15:28:00 -0400

    code cleaning

--- main.py
+++ main.py
@@ -10,62 +10,92 @@ in the output layer.
 @author: Abdel
 """
 
-import pandas as pd
+#------------------------------------------------------------------------------
+#If in google colab
+
+IN_COLAB = False
+
+if IN_COLAB:
+    from google.colab import drive
+    import nltk
+    drive.mount('/content/gdrive/')
+    nltk.download('punkt')
+    PATH = 'gdrive/My Drive/IFT3150/'
+
+else:
+    PATH = ''
+
+
+
+#------------------------------------------------------------------------------
+#Load files
 
 def load_src(name, fpath):
     import os, imp
     return imp.load_source(name, os.path.join(os.path.dirname(__file__), fpath))
 
-load_src("data_utils", "data/utils.py" )
-load_src("embeddor", "embeddor/BinaryEmbeddor.py" )
-load_src("modelisor_", "modelisor/modelisor.py" )
-load_src("modelisor_utils", "modelisor/utils.py" )
+load_src("data_utils", PATH+"data/utils.py" )
+load_src("embeddor", PATH+"embeddor/BinaryEmbeddor.py" )
+load_src("modelisor_", PATH+"modelisor/modelisor.py" )
+load_src("modelisor_utils", PATH+"modelisor/utils.py" )
 
-
-USE_PRETRAIN_LOWDIM_EMBS = True  #False to retrain binary embeddings
-WORD_EMBEDDINGS_DIM = 100 #25,50,100,200 values
-
-
+#------------------------------------------------------------------------------
 #Load data
+
+import pandas as pd
 from data_utils import get_twitter_dataset
 
-data = get_twitter_dataset("data/tweet.tsv")
-sentences, labels = data["input"], data["target"]
+data = get_twitter_dataset("data/training.1600000.processed.noemoticon.csv")
+data = data.sample(frac=1)
+sentences, labels = data["text"], data["target"]
 
+#------------------------------------------------------------------------------
 #Getting pretrained word embeddings and vocab
+
 import gensim
 
-emb_path = 'word_embeddings/w2v.twitter.27B.' + str(WORD_EMBEDDINGS_DIM)+ 'd.txt'
-model = gensim.models.KeyedVectors.load_word2vec_format(emb_path)
+WORD_EMBEDDINGS_DIM = 100 #25,50,100,200 values
+
+word_emb_path = 'word_embeddings/w2v.twitter.27B.' + str(WORD_EMBEDDINGS_DIM)+ 'd.txt'
+model = gensim.models.KeyedVectors.load_word2vec_format(PATH+word_emb_path)
 vocab = model.vocab
 
+#------------------------------------------------------------------------------
+#Get idx and BoW representation of inputs (idxs and bagofword)
 
-#Get idx and BoW representation of inputs
-from data_utils import TwitterPreprocessorTokenizer,  text_to_sequence, text_to_bow
+from data_utils import TwitterPreprocessorTokenizer, text_to_sequence, text_to_bow
 
 tokenizer = TwitterPreprocessorTokenizer(stem=False,stopwords=True)
 word_idxs = text_to_sequence(sentences, tokenizer, vocab)
 bows = text_to_bow(sentences, tokenizer)
 
-
+#------------------------------------------------------------------------------
 #Get the binary low dim sentence representation (embedded sentences)
-from embeddor import BinaryEmbeddor
 
-emb_path = 'embeddor/embeddings.csv'
-if USE_PRETRAIN_LOWDIM_EMBS:  
-    lowdim_sentences_embs = pd.read_csv(emb_path, sep=',').values
+USE_PRETRAIN_BIN_EMBS = False
+BIN_DIM = 20
+
+bin_emb_path = 'embeddor/bin_embs_'+str(BIN_DIM)+'d.csv'
+
+if USE_PRETRAIN_BIN_EMBS:  
+    lowdim_sentences_embs = pd.read_csv(PATH+bin_emb_path, sep=',').values
 else:
-    lowdim_sentences_embs = BinaryEmbeddor()(bows.todense())
-    pd.DataFrame(lowdim_sentences_embs).to_csv(emb_path, index=False)
+    from embeddor import BinaryEmbeddor
+    lowdim_sentences_embs = BinaryEmbeddor(bin_dim=BIN_DIM)(bows.todense())
+    pd.DataFrame(lowdim_sentences_embs).to_csv(PATH+bin_emb_path, index=False)
 
-
-
+#------------------------------------------------------------------------------
 #Simple train / valid split
-X_train, y_train = word_idxs[:6000], lowdim_sentences_embs[:6000]
-X_test, y_test = word_idxs[6000:], lowdim_sentences_embs[6000:]
+    
+TRAIN_SIZE=0.8
+DEL = int(len(word_idxs)*TRAIN_SIZE)
+X_train, bin_train = word_idxs[:DEL], lowdim_sentences_embs[:DEL]
+X_test, bin_test = word_idxs[DEL:], lowdim_sentences_embs[DEL:]
 
-
+#------------------------------------------------------------------------------
 #Training the CNN to get lowdim sentence modelisation
+
+
 import torch
 import torch.optim as optim
 from torch.nn import BCELoss
@@ -73,19 +103,46 @@ from torch.nn import BCELoss
 from modelisor_ import CNN
 from modelisor_utils import train_model
 
-cnn_modelisor = CNN(vocab_size=len(vocab), sentence_length=word_idxs.shape[1], \
-                    emb_weights=torch.FloatTensor(model.vectors))
-criterion = BCELoss()
-optimizer = optim.Adam(cnn_modelisor.parameters(), lr=0.0001)
+#Hyperparameters
+EPOCHS = 20
+BATCH_SIZE = 16
+K_TOP = 3
+LEARNING_RATE = 0.001
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-cnn_modelisor.to(device=device)
+cnn_modelisor = CNN(vocab_size=len(vocab), sentence_length=word_idxs.shape[1], \
+                    emb_weights=torch.FloatTensor(model.vectors),  k_top=K_TOP)
+cnn_modelisor.to(device)
 
-for epoch in range(100):
-    a = train_model(cnn_modelisor, X_train, y_train, X_test, y_test, 
-                    criterion, optimizer, device, batch_size=32)
-    print(a)
+criterion = BCELoss()
+optimizer = optim.Adam(cnn_modelisor.parameters(), lr=LEARNING_RATE)
+
+losses, losses_valid = [],[]
+for epoch in range(EPOCHS):
+    loss, loss_valid = train_model(cnn_modelisor, X_train, bin_train, X_test, bin_test, 
+                    criterion, optimizer, device, batch_size=BATCH_SIZE)
+    
+    losses.append(loss)
+    losses_valid.append(losses)
+    plot(losses, losses_valid)
+    
+    
+    
+
+X_train_reduced = cnn_modelisor.get_hidden_representation(torch.tensor(X_train, dtype=torch.long))
 
 
+#------------------------------------------------------------------------------
+#Utils
 
+
+def plot(losses, losses_valid):
+    import matplotlib.pyplot as plt
+    
+    plt.plot(losses, label="train loss")
+    plt.plot(losses_valid, label="valid loss")
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.show()
+    
